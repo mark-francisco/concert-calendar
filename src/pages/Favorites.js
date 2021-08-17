1cdf00e01801bdc0dc2f47cd5c823e07733474cb
Mark Francisco <mfrancisco581@gmail.com>
2021-08-17 15:27:13 -0500

    show favs in Favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+
+import ConcertList from "../components/concerts/ConcertList";
+
 function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You don't have any favorites yet.</p>;
+  } else {
+    content = <ConcertList concerts={favoritesCtx.favorites} />;
+  }
+
   return (
     <section>
       <h1>My Favorites:</h1>
+      {content}
     </section>
   );
 }
